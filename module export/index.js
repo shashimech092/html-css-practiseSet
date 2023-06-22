6b1f668ea11ab2cb2e8eb4aa4ff42cdd2c4e9db7
Dr. Shashikant <shashimech092@gmail.com>
2023-06-22 14:56:08 +0530

    first commit

---
+++ module export/index.js
@@ -0,0 +1,6 @@
+// const mod = require("./mod");
+// console.log(mod.average(3,4))
+const mod = require("./mod");
+console.log(mod.name)
+// console.log(mod.avg([3,4]))
+// console.log("This is index.js")
